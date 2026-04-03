8d15dad20491e3a508bfa5925995cfbecd798529
juma-paul <raisben7@gmail.com>
2026-04-03 15:07:47 -0700

    feat(api): add user and auth API functions

---
+++ src/lib/user.api.ts
@@ -0,0 +1,41 @@
+import api from "@/lib/api";
+
+// Profile
+export const getProfile = () => api.get("/users/profile");
+
+export const updateProfile = (data: {
+  first_name?: string;
+  last_name?: string;
+  avatar_url?: string;
+}) => api.put("/users/profile", data);
+
+// Email & Password
+export const changeEmail = (newEmail: string, password: string) =>
+  api.put("/users/change-email", { newEmail, password });
+
+export const changePassword = (
+  currentPassword: string,
+  newPassword: string,
+  confirmPassword: string,
+) =>
+  api.put("/users/change-password", {
+    currentPassword,
+    newPassword,
+    confirmPassword,
+  });
+
+// 2FA
+export const setup2FA = () => api.post("/users/2fa/setup");
+
+export const verify2FA = (code: string) =>
+  api.post("/users/2fa/verify", { code });
+
+export const disable2FA = (code: string) =>
+  api.post("/users/2fa/disable", { code });
+
+// Account
+export const deleteAccount = (password: string) =>
+  api.delete("/users/account", { data: { password } });
+
+export const restoreAccount = (token: string) =>
+  api.post("/users/account/restore", { token });
